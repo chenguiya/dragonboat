97717dbb2119f20d2d4408c6a995bc7f5761815a
hua <965781546@qq.com>
2015-10-26 00:01:34 +0800

    add

---
+++ config/config_ucenter.php
@@ -0,0 +1,20 @@
+<?php
+
+
+define('UC_CONNECT', 'mysql');
+
+define('UC_DBHOST', 'localhost');
+define('UC_DBUSER', 'root');
+define('UC_DBPW', 'root');
+define('UC_DBNAME', 'dragonboat');
+define('UC_DBCHARSET', 'utf8');
+define('UC_DBTABLEPRE', '`dragonboat`.dragon_ucenter_');
+define('UC_DBCONNECT', 0);
+
+define('UC_CHARSET', 'utf-8');
+define('UC_KEY', 'B4EaY9jbJ1G2G1vdPcv01eP8Q9y1Y7jeD1y0LfB41cL4Wb58U82fh9Zen1van2o2');
+define('UC_API', 'http://www.asiandragonboat.com/uc_server');
+define('UC_APPID', '1');
+define('UC_IP', '');
+define('UC_PPP', 20);
+?>
